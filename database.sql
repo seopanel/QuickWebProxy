7c536d2f4ba573f1cd5eab775c79e24a2fd02cf4
Geo Varghese <sendtogeo@gmail.com>
2018-05-24 00:53:50 +0530

    db changes

--- database.sql
+++ database.sql
@@ -14,7 +14,8 @@ CREATE TABLE IF NOT EXISTS `qwp_settings` (
 
 INSERT INTO `qwp_settings` (`set_label`, `set_name`, `set_val`, `set_type`) VALUES
 ('Allow user to access the web proxy', 'QWP_ALLOW_USER_WEB_PROXY', '0', 'bool'),
-('Allow web server to act as a proxy', 'QWP_ALLOW_WEB_SERVER_ACT_AS_PROXY', '1', 'bool');
+('Allow web server to act as a proxy', 'QWP_ALLOW_WEB_SERVER_ACT_AS_PROXY', '1', 'bool') 
+ON DUPLICATE KEY UPDATE `set_type`=`set_type`;
 
 INSERT INTO `texts` (`lang_code`, `category`, `label`, `content`) VALUES
 ('en', 'QuickWebProxy', 'Anonymize', 'Anonymize'),
@@ -24,4 +25,5 @@ INSERT INTO `texts` (`lang_code`, `category`, `label`, `content`) VALUES
 ('en', 'QuickWebProxy', 'Web Server', 'Web Server'),
 ('en', 'QuickWebProxy', 'Please enter a valid url', 'Please enter a valid url'),
 ('en', 'QuickWebProxy', 'Server list is empty', 'Server list is empty'),
-('en', 'common', 'Server', 'Server');
+('en', 'common', 'Server', 'Server') 
+ON DUPLICATE KEY UPDATE `changed`=`changed`;
