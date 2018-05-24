a25c989af5c5c50ef83c20ed0008d0e3ba1c54d2
Geo Varghese <sendtogeo@gmail.com>
2018-05-25 01:16:57 +0530

    hide reports for users

--- themes/classic/views/menu.ctp.php
+++ themes/classic/views/menu.ctp.php
@@ -6,9 +6,9 @@ $spTextPanel = $pluginCtrler->getLanguageTexts('panel', $_SESSION['lang_code']);
 <ul id='subui'>
 	<?php if(isAdmin() || QWP_ALLOW_USER_WEB_PROXY) {?>
     	<li><a href="javascript:void(0);" onclick="<?php echo  pluginMenu(); ?>"><?php echo  $pluginText['Web Proxy']?></a></li>
-    	<li><a href="javascript:void(0);" onclick="scriptDoLoad('log.php?sec=crawl&crawl_type=webproxy', 'content');"><?php echo $spText['common']['Reports']?></a></li>
 	<?php }?>
-    <?php if(isAdmin()) {?>	
+    <?php if(isAdmin()) {?>
+    	<li><a href="javascript:void(0);" onclick="scriptDoLoad('log.php?sec=crawl&crawl_type=webproxy', 'content');"><?php echo $spText['common']['Reports']?></a></li>	
 		<li><a href="javascript:void(0);" onclick="<?php echo pluginMenu('action=settings'); ?>"><?php echo $spTextPanel['Settings']?></a></li>
     <?php }?>	
 	<li><a href="javascript:void(0);" onclick="<?php echo  pluginMenu('action=aboutus'); ?>"><?php echo  $spTextPanel['About Us']?></a></li>
