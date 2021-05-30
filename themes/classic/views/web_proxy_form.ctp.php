03ec01713f0fa6b7978446f4eef506e0e6377f23
geo varghese <geo.varghese@c.thycotic.com>
2021-05-31 02:53:26 +0530

    new version of QuickWebProxy 2.0 using php proxy app

--- themes/classic/views/web_proxy_form.ctp.php
+++ themes/classic/views/web_proxy_form.ctp.php
@@ -3,7 +3,7 @@ echo showSectionHead($pluginText['Web Proxy']);
 $actFun = SP_DEMO ? "alertDemoMsg()" : pluginConfirmPOSTMethod('projectform', 'subcontent', 'action=doWebProxy');
 ?>
 <form id='projectform' onsubmit="<?php echo $actFun?>;return false;">
-<table class="search">	
+<table class="search" style="width: 80%">	
 	<tr>
 		<th><?php echo $spText['common']['Server']?>:</th>
 		<td>
@@ -21,12 +21,12 @@ $actFun = SP_DEMO ? "alertDemoMsg()" : pluginConfirmPOSTMethod('projectform', 's
 	<tr>
 		<th><?php echo $spText['common']['Url']?>:*</th>
 		<td>
-			<input type="text" name="url" style="width: 400px;" value="<?php echo $post['url']?>">
+			<input type="text" name="url" value="<?php echo $post['url']?>" class="form-control">
 		</td>
 	</tr>
 	<tr>
 		<th>&nbsp;</th>
-		<td style="padding-left: 9px;">
+		<td style="padding: 10px;">
 			<a href="javascript:void(0);" onclick="<?php echo $actFun?>" class="actionbut"><?php echo $spText['button']['Proceed']?></a>
 		</td>
 	</tr>
